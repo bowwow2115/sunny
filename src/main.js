b825a284d58c94c86d0f2604ba80436dd075960a
SeungHunPark <bowwow94@inzent.com>
2024-06-17 18:12:43 +0900

    코어 소스 및 필요 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -2,12 +2,94 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import store from './store';
+
+// import ErrorDialog from "@/components/custom/dialog/ErrorDialog";
+import VueClipboard from 'vue-clipboard2'
+import Constants from './Constants'
+import Utils from '@/utils/utils'
+import * as ModalDialogs from 'vue-modal-dialogs'
+import lodash from 'lodash'
+
+window.$ = require('jquery');
+window.jQuery = require('jquery');
+window.store = store;
+
+window.constants = Constants;
+Vue.prototype.$Constants = Constants
+Vue.config.productionTip = false;
 
 Vue.config.productionTip = false;
 
-new Vue({
+VueClipboard.config.autoSetContainer = true;
+
+Vue.prototype.$lodash = lodash
+Vue.prototype.$Utils = Utils;
+window.Utils = Utils;
+Vue.use(ModalDialogs)
+Vue.use(VueClipboard);
+
+
+// async function openErrorDialog(errData) {
+//   const errorDialog = ModalDialogs.create(ErrorDialog);
+//   await errorDialog({
+//       code: errData.code,
+//       message: errData.message
+//   });
+// }
+
+// Vue.prototype.$showErrMsgbox = function(errData = null) {
+//   openErrorDialog(errData);
+// }
+
+Vue.prototype.$showError = function(error = null, title = null) {
+  let message = '';
+  if (error && error.status && error.error) {
+      message = error.status + ' ' + error.error;
+  }
+  if (!error)
+      message = "처리가 실패하였습니다. \n오류 내용은 관리자에게 전달되었습니다.";
+
+  if (error && error.code) {
+      message = `${error.message} (${error.code})`
+  }
+
+  console.log(message)
+
+  if (!title)
+      title = "오류";
+
+  this.$notify.error({
+      position: 'bottom-right',
+      duration: 2000,
+      title: title,
+      message: message
+  })
+}
+Vue.prototype.$showOk = function(message = null) {
+  if (!message)
+      message = "처리가 완료되었습니다.";
+
+  this.$notify.success({
+      position: 'bottom-right',
+      duration: 1000,
+      title: "성공",
+      message: message,
+      offset: 60
+  });
+}
+
+if (process.env.NODE_ENV === 'production') {
+  window._consolelog = console.log
+  console.log = function() {}
+}
+window.console = console
+
+let vue = new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
 
+window.vue = vue;
