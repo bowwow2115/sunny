3edc6ce4c328b70f4f6f717a3246f8189c74fecc
bowwow2115 <bowwow2115@gmail.com>
2024-06-20 11:22:37 +0900

    eslint, prettier 설정 추가

--- src/components/signIn.vue
+++ src/components/signIn.vue
@@ -8,7 +8,9 @@
           v-model="form.userId"
           label="아이디 입력"
           prepend-inner-icon="ri-user-line"
-          required outlined clearable
+          required
+          outlined
+          clearable
         ></v-text-field>
         <!-- 비밀번호 -->
         <v-text-field
@@ -16,10 +18,17 @@
           type="password"
           label="비밀번호 입력"
           prepend-inner-icon="ri-lock-line"
-          required outlined clearable
+          required
+          outlined
+          clearable
         ></v-text-field>
-        <v-checkbox v-model="form.saveLogin" label="로그인 상태유지"></v-checkbox>
-        <v-btn type="submit" block depressed color="primary" class="btn-submit">로그인</v-btn>
+        <v-checkbox
+          v-model="form.saveLogin"
+          label="로그인 상태유지"
+        ></v-checkbox>
+        <v-btn type="submit" block depressed color="primary" class="btn-submit"
+          >로그인</v-btn
+        >
         <div class="login-more">
           <router-link to="/signUp">회원가입</router-link>
           <span>|</span>
@@ -31,9 +40,8 @@
 </template>
 
 <script>
-import { methods } from 'babel-plugin-transform-runtime/lib/definitions';
-import auth from "@/api/auth";
-
+import { methods } from 'babel-plugin-transform-runtime/lib/definitions'
+import auth from '@/api/auth'
 
 export default {
   name: 'signIn',
@@ -42,27 +50,28 @@ export default {
     return {
       valid: true,
       form: {
-        userId: "",
-        password: "",
-        saveLogin: "",
+        userId: '',
+        password: '',
+        saveLogin: '',
       },
     }
   },
   methods: {
-        login() { //로그인 폼 submit
-          auth
-          .login(this.form)
-          .then((_) => {
-            this.$router.push({ path: constants.DEFAULT_HOME });
-          })
-          .catch((error) => {
-            console.log(error);
-            // this.$message({
-            //   type: "info",
-            //   message: error.message,
-            // });
-          })
-        }
-      },
-};
+    login() {
+      //로그인 폼 submit
+      auth
+        .login(this.form)
+        .then((_) => {
+          this.$router.push({ path: constants.DEFAULT_HOME })
+        })
+        .catch((error) => {
+          console.log(error)
+          // this.$message({
+          //   type: "info",
+          //   message: error.message,
+          // });
+        })
+    },
+  },
+}
 </script>
