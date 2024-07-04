fdc30de7c7681069593dd483819bc699f8d1dc22
SeungHunPark <bowwow94@inzent.com>
2024-07-04 18:51:30 +0900

    더보기 버튼 추가

--- src/components/dialog/ParentsRideInfo.vue
+++ src/components/dialog/ParentsRideInfo.vue
@@ -1,11 +1,67 @@
 <template>
-  <v-dialog v-model="errorMsgBox.show" @input="onDialogInput" max-width="400px">
+  <v-dialog v-model="infoBox.show" persistent max-width="600px">
     <v-card>
-      <v-card-title class="headline">{{ errorMsgBox.code }}</v-card-title>
-      <v-card-text>{{ errorMsgBox.message }}</v-card-text>
+      <v-card-title>
+        <span class="text-h5">부모 & 차량정보</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="4" sm="2" md="1">
+              <v-text-field label="이름" required></v-text-field>
+            </v-col>
+            <v-col cols="4" sm="2" md="1">
+              <v-text-field label="Legal middle name"></v-text-field>
+            </v-col>
+            <v-col cols="4" sm="2" md="1">
+              <v-text-field
+                label="Legal last name*"
+                persistent-hint
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Email*" required></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                label="Password*"
+                type="password"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select
+                :items="['0-17', '18-29', '30-54', '54+']"
+                label="Age*"
+                required
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-autocomplete
+                :items="[
+                  'Skiing',
+                  'Ice hockey',
+                  'Soccer',
+                  'Basketball',
+                  'Hockey',
+                  'Reading',
+                  'Writing',
+                  'Coding',
+                  'Basejump',
+                ]"
+                label="Interests"
+                multiple
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+        </v-container>
+        <small>*indicates required field</small>
+      </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="red lighten-2" text @click="closeError">닫기</v-btn>
+        <v-btn color="blue darken-1" text @click="closeInfo"> Close </v-btn>
+        <v-btn color="blue darken-1" text @click="closeInfo"> Save </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -13,25 +69,28 @@
 
 <script>
 export default {
+  name: 'ParentRideInfo',
   data() {
     return {
-      errorMsgBox: {
+      infoBox: {
         show: false,
-        code: '',
-        message: '',
+        parentList: [],
+        amRide: {},
+        pmRide: {},
       },
     }
   },
   methods: {
-    showError(payload) {
-      this.errorMsgBox.show = true
-      this.errorMsgBox.code = payload.code || '정의되지 않은 에러입니다.'
-      this.errorMsgBox.message = payload.message || '관리자에게 문의하세요'
+    showInfo(payload) {
+      this.infoBox.show = true
+      this.infoBox.parentList = payload.parentList
+      this.infoBox.amRide = payload.amRide
+      this.infoBox.pmRide = payload.pmRide
     },
-    closeError() {
-      this.errorMsgBox.show = false
-      this.errorMsgBox.code = ''
-      this.errorMsgBox.message = ''
+    closeInfo() {
+      this.infoBox.show = false
+      this.infoBox.code = ''
+      this.infoBox.message = ''
     },
     onDialogInput(value) {
       if (!value) {
