e2743c4d629bca5d7a7ec87aadc027370afd3524
bowwow2115 <bowwow2115@gmail.com>
2024-08-14 01:00:51 +0900

    반 설정 추가

---
+++ src/components/dialog/ManageClassDialog.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-dialog
+    v-model="visible"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+    max-width="100%"
+  >
+    <v-card>
+      <v-toolbar color="primary" dark>
+        <v-toolbar-title>
+          {{ '반 설정' }}
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon dark @click="cancel">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list>
+        <v-list-group
+          :value="true"
+          prepend-icon="mdi-account-box-multiple"
+          :no-action="true"
+        >
+          <template v-slot:activator>
+            <v-list-item-subtitle>반</v-list-item-subtitle>
+          </template>
+          <div v-if="form.classList.length != 0">
+            <v-list-item-group
+              v-for="(sunnyClass, index) in form.classList"
+              :key="index"
+            >
+              <v-list-item style="padding-left: 15%; padding-right: 8%">
+                <v-list-item-content>
+                  <v-list-item-title>{{ sunnyClass.name }}</v-list-item-title>
+                  <!-- <v-spacer></v-spacer> -->
+                </v-list-item-content>
+                <v-list-item-icon @click="deleteClass(sunnyClass.id)">
+                  <v-icon color="red darken3">mdi-minus</v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+              <v-divider v-if="index != form.classList.length - 1"></v-divider>
+            </v-list-item-group>
+          </div>
+          <v-list-item v-else style="padding-left: 15%; padding-right: 8%">
+            <v-list-item-icon>
+              <v-icon>mdi-information-off</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                등록된 반의 정보가 없습니다.
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item style="padding: 0px">
+            <v-spacer></v-spacer>
+            <v-btn @click="openAddClassDialog()"
+              ><v-icon color="green darken3">mdi-plus</v-icon></v-btn
+            >
+            <v-spacer></v-spacer>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { getClassList } from '@/api/api'
+import ManageMeetingLocationDialog from '@/components/dialog/ManageMeetingLocationDialog.vue'
+import SearchChildDialog from '@/components/dialog/SearchChildDialog.vue'
+export default {
+  mounted() {
+    this.getClassList()
+  },
+  data() {
+    return {
+      visible: false,
+      resolve: null,
+      reject: null,
+      form: {
+        classList: [],
+      },
+      isEdit: false,
+    }
+  },
+  methods: {
+    open() {
+      this.visible = true
+      return new Promise((resolve, reject) => {
+        this.resolve = resolve
+        this.reject = reject
+      })
+    },
+    cancel() {
+      this.visible = false
+      this.resolve(true)
+    },
+    async deleteClass(childRide) {
+      const result = await this.$confirm({
+        message: `${childRide.child.name}을 정말 ${this.form.name}에서 제외시키겠습니까?`,
+      })
+      if (result) {
+        this.$withLoading(
+          deleteClass(childRide.id)
+            .then((response) => {
+              if (response.code == '0') {
+                this.$showMessage({
+                  type: 'success',
+                  message: '성공적으로 삭제했습니다.',
+                })
+                let index = this.form.childRideList.findIndex(
+                  (item) => item.id === childRide.id
+                )
+                if (index !== 1) {
+                  this.$delete(this.form.childRideList, index)
+                }
+              }
+            })
+            .catch((e) => {
+              this.$showError(e)
+            })
+        )
+      }
+    },
+    async openAddClassDialog() {
+      //원아정보 전달 시 현재 코스에 등록되어있는 원아 필터링 후 넘김(여기서 검색)
+      let item = {}
+      item.meetingLocationName = this.form.name
+      item.meetingLocationId = this.form.id
+      item.childList = await this.getAllChildren()
+      const result = await this.$dialog(SearchChildDialog, item)
+      console.log(result)
+      if (result != null) {
+        this.$showMessage({
+          type: 'success',
+          message: '성공적으로 추가했습니다.',
+        })
+        for (let item of result) {
+          this.$set(
+            this.form.childRideList,
+            this.form.childRideList.length,
+            item
+          )
+        }
+      }
+    },
+    getClassList() {
+      getClassList()
+        .then((response) => {
+          if (response.code == '0') {
+            this.form.classList = response.data
+          }
+        })
+        .catch((e) => {
+          this.$showError(e)
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* 원하는 스타일 추가 */
+</style>
