836fe8461d2e728b3f03e1d84be227d5540dd4fb
bowwow2115 <bowwow2115@gmail.com>
2024-08-14 01:00:51 +0900

    반 설정 추가

--- src/components/admin/AdminMenu.vue
+++ src/components/admin/AdminMenu.vue
@@ -2,20 +2,35 @@
   <v-container>
     <v-row>
       <v-col>
-        <button>반 설정</button>
+        <v-btn @click="openManageClassDialog()">반 설정</v-btn>
       </v-col>
       <v-col>
-        <button>승하차장소 설정</button>
+        <v-btn>승하차장소 설정</v-btn>
       </v-col>
       <v-col>
-        <button>유저설정</button>
+        <v-btn>유저설정</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-export default {}
+import ManageClassDialog from '@/components/dialog/ManageClassDialog.vue'
+
+export default {
+  name: 'AdminMenu',
+  mounted() {},
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    async openManageClassDialog() {
+      await this.$dialog(ManageClassDialog, null)
+    },
+  },
+}
 </script>
 
 <style></style>
