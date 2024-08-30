a800da5a0f90e1e906ec129115dd0cee6e2b0ed6
bowwow2115 <bowwow2115@gmail.com>
2024-08-30 18:25:20 +0900

    화면 너비 고정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <v-container>
+    <!-- TODO: 정식 배포 시 밑 주석 <v-btn v-if="isAdmin" @click="goToAdminMenu">어드민 페이지</v-btn> -->
+    <v-btn @click="goToAdminMenu">어드민 페이지</v-btn>
     <v-col cols="12">
       <v-card>
         <v-card-title>이달의 생일자</v-card-title>
