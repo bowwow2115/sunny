bfbf514fcc7d13f720b3cea7ed41a129431f4444
yujeong92 <namurj3@gmail.com>
2024-09-20 00:09:04 +0900

    학교안심 웹폰트 추가, 홈화면 진행중

--- src/views/SunnyHome.vue
+++ src/views/SunnyHome.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
+  <div class="_home">
+    <v-row>
+      <v-col md="6">
+        <v-card class="pa-2 rounded-xl _log">
+          <v-card-title class="_logo"><img src="" alt="" /></v-card-title>
+          <v-card-text>
+            <h2>000 님, 환영합니다.</h2>
+          </v-card-text>
+
+          <v-card-actions class="justify-space-between">
+            <v-btn type="button" outlined rounded text x-large color="gray"
+              >로그아웃</v-btn
+            >
+            <v-btn type="button" outlined rounded text x-large
+              >마이페이지</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col md="6">
+        <v-card class="pa-2 rounded-xl _birth">
+          <v-card-title class="_font-miso font-weight-bold"
+            >오늘의 생일</v-card-title
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+
     <v-row style="height: 50vh">
       <v-col cols="6" xs="12">
         <v-card style="height: 50vh">
